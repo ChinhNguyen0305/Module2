30fd1111fe04811231858ca5be93d63f7ef9d07b
VO HUU NHAT DUC <vohuunhatduc@VOs-MacBook-Air.local>
2021-07-27 09:04:49 +0700

    ok

---
+++ lesson1/newproject/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        byte i = 10;
+        float f=20.5f;
+        double d = 20.5;
+        boolean b = true;
+        String c = "c";
+        String s = "Hà Nội";
+
+
+    }
+}
